24b11b4f9215c6da6c380d74c4edbe9db175d81f
Navyush-RanaTx <navyush.rana@testingxperts.com>
2025-05-08 11:05:41 +0530

    add files via upload

---
+++ my-web-app/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 80;
+
+app.get('/', (req, res) => {
+  res.send('Hello from Dockerized Node.js App!');
+});
+
+app.get('/health', (req, res) => {
+  res.send('OK');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
